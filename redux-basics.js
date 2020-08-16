761ea8e90ca9a0e65bd43e81966f01228450fbf3
Gurveen <gurveens11@gmail.com>
2020-08-16 21:41:47 +0530

    Added Subscription

--- redux-basics.js
+++ redux-basics.js
@@ -27,9 +27,12 @@ const rootReducer = (state = intialState, action) => {
 //Store
 const store = createStore(rootReducer);
 
+//Subscription
+store.subscribe(() => {
+	console.log('[Subscription]', store.getState());
+});
+
 //Dispatching Action
 store.dispatch({ type: 'INC_COUNTER' });
 store.dispatch({ type: 'ADD_COUNTER', value: 10 });
 console.log(store.getState());
-
-//Subscription
