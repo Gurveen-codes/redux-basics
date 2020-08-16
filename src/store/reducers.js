639b1b6959269462b274a5fb5851d8d61102be29
Gurveen <gurveens11@gmail.com>
2020-08-16 22:48:32 +0530

    Set up Store, Reducer, Subscription

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -3,6 +3,9 @@ const intialState = {
 };
 
 const reducer = (state = intialState, action) => {
+	if (action.type === 'INCREMENT') {
+		return { ...state, counter: state.counter + 1 };
+	}
 	return state;
 };
 
